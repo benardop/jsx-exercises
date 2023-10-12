16854634aa239d69a93f70ca0a6a3fa6c829a363
benardop <benard.oc.p@gmail.com>
2023-10-12 05:44:25 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function MyThing() {
+  return (
+    <div>
+      Hello, World!
+    </div>
+  )
+}
+
+function Book() {
+  return (
+    <div className='book'>
+      <div className='title'>
+        The Title
+      </div>
+      <div className='author'>
+         The Author
+      </div>
+      <ul className='stats'>
+         <li className='rating'>
+           5 stars
+         </li>
+         <li className='isbn'>
+           12-345678-22
+         </li>
+      </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div>
+    <MyThing /> <Book/>
+  </div>
+)
